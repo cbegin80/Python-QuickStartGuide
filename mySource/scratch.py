899da1386eb502a2833239f76bbf33c86e088c91
Chris Begin <cbegin80@gmail.com>
2025-04-26 02:36:26 -0500

    complete several more small exercises

---
+++ mySource/scratch.py
@@ -0,0 +1,6 @@
+print("Hello,\nWorld!\n\nHello, World!")
+
+# Exercise: ask the user for their name and print the first letter
+
+name = input("What is your first name? ")
+print("Your first initial is " + name[0])
