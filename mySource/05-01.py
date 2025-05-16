b59688477601145041c2c9dcb949341d311e6240
Chris Begin <cbegin80@gmail.com>
2025-05-15 20:30:11 -0500

    Auth Error

---
+++ mySource/05-01.py
@@ -0,0 +1,8 @@
+# Define the ask function
+def ask(prompt):
+    return input(prompt + " ")
+
+# Use the ask function to find out how many cups we want
+
+question = ask("How many cups do you want?")
+print(question)
