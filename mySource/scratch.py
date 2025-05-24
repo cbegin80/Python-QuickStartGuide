edfafb4cdc60106464396f4bb09939436462fec9
Chris Begin <cbegin80@gmail.com>
2025-05-24 10:39:27 -0500

    syncing between computers

--- mySource/scratch.py
+++ mySource/scratch.py
@@ -1,11 +1,4 @@
-from dataclasses import dataclass
+import locale
 
-@dataclass
-class Customer:
-    name: str
-    city: str = "Florence"
-    bonus_points: int = 100
-    total_spent: float = 0.00
-
-c1 = Customer("Robert")
-print(repr(c1))
+for l in locale.locale_alias:
+    print(l)
