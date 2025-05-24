e7640da3047cc97e6d66c5356d8b42f44f66ba0f
Chris Begin <cbegin80@gmail.com>
2025-05-24 10:57:21 -0500

    Through page 180 / just prior to 'Input Validation'

---
+++ mySource/CH10/10-04.py
@@ -0,0 +1,5 @@
+# Create our string
+tongue_twister = "She sells seashells by the seashore."
+
+# Count the number os 's' in tongue_twister and display it
+print (f"There are {tongue_twister.count("s")} letter s in: \"{tongue_twister}\"")
---
+++ mySource/CH10/10-05.py
@@ -0,0 +1,8 @@
+# A simple string
+fox = "The quiick brown fox jumps over the lazy dog."
+
+# Split the string
+fox_list = fox.split()
+
+# Display the resulting list
+print(fox_list)
