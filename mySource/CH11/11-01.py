81dc7396af665f4cbd6a9633e76f96f6a504527e
Chris Begin <cbegin80@gmail.com>
2025-06-07 22:35:21 -0500

    Start of CH11

---
+++ mySource/CH11/11-01.py
@@ -0,0 +1,14 @@
+# import the math module
+import math
+
+# Ask the user for a number
+number = input("Please enter a number: ")
+
+# Convert to int
+number = int(number)
+
+# Calculate result
+result = math.sqrt(number)
+
+# Display result
+print("The square root of {:n} is {:6f}".format(number, result))
